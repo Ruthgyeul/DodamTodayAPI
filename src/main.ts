ef22afdb9f58386cb19f0adaef08574bbb2f7d30
Ruthgyeul <wingd4705@gmail.com>
2025-01-30 21:23:04 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cors from 'cors';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000', // Allow Next.js frontend
+      credentials: true,
+    })
+  );
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
